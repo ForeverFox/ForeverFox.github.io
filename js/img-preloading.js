d0b70c3927d1c9ea6903c93fd629890cc998f55e
Forever Fox <cogden@mcad.edu>
2018-10-05 18:35:55 +0300

    Preloading Update

--- js/img-preloading.js
+++ js/img-preloading.js
@@ -1,24 +1,9 @@
-/* Image Preloading 
-var happyImages = new Array(
-    "../img/map-icon.png",    
-    "../img/map.png",
-    "../img/about-brochure.png",
-    "../img/albatross.png",
-    "../img/resources.png"
-    );
-
-function imgPreload(){
-  for (i = 0; i < imgPreload.arguments.length; i++) {
-   happyImages[i] = new Image();
-   happyImages[i].src = imgPreload.arguments[i];
-  };
-
-imgPreload();
-
-};*/
-
 $(function() {
-  $(".preload-images").ready(function() {
+  $(".preload-images").waitForImages(function() {
+    console.log("All images loaded.");
     $("#preloader").fadeOut("fast");
+    $("#preloader").remove();    
+    $("#home").fadeIn("slow");
+    $("#preload-images").remove();    
   });
 });
