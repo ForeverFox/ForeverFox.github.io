1da592f9a1b1d3b08df8f7d713e26a1fe96be18c
Forever Fox <cogden@mcad.edu>
2018-10-09 18:34:29 +0300

    Preloading Update

--- js/img-preloading.js
+++ js/img-preloading.js
@@ -1,10 +1,12 @@
 $(function() {
   $("#preload-images").waitForImages(function() {
       console.log("All images loaded.");
-      $("#preloader").fadeOut("slow", function() {
+      
+      $("#preloader").fadeOut("2000", function() {
       $(this).remove();
-      });    
-      $("#home").fadeIn("slow");
+      }); 
+
+      $("#home").fadeIn("3000");
       $("#preload-images").remove();
     });
 });
