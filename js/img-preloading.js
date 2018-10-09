eb4e4106081f2aa61541e5867e6151c8993a0f13
Forever Fox <cogden@mcad.edu>
2018-10-09 19:56:00 +0300

    Preloading Edits

--- js/img-preloading.js
+++ js/img-preloading.js
@@ -3,10 +3,17 @@ $(function() {
 	    finished: function() {
 	        console.log("All images loaded.");
 			$("#preloader").delay(4000).fadeOut(500);
+			$("#home").addClass("loaded");
 		},
 	    each: function() {
        		console.log("Image loaded.")
 	    },
 	    waitForAll: true
 	});
+
+	if ($("#home").hasClass("loaded")) {
+		$("#fish").show();
+	} else {
+		return false;
+	}
 });
